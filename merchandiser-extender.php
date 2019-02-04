98f419f1b757fbd206a8a0b1dde9315a0ab64fe4
Vanesa Reinerth <vanesa@getbowtied.com>
2019-02-04 16:44:53 +0200

    v.1.2.1

--- merchandiser-extender.php
+++ merchandiser-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Merchandiser Extender
  * Plugin URI:        		https://merchandiser.wp-theme.design/
  * Description:       		Extends the functionality of Merchandiser with Gutenberg elements.
- * Version:           		1.2
+ * Version:           		1.2.1
  * Author:            		GetBowtied
  * Author URI:        		https://getbowtied.com
  * Requires at least: 		5.0
