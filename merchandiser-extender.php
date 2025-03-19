ded719f23eeba40f859d411333fb6958f5772bef
Romi H. <romi@getbowtied.com>
2025-03-19 14:20:17 +0200

    2.7

--- merchandiser-extender.php
+++ merchandiser-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Merchandiser Extender
  * Plugin URI:        		https://merchandiser.wp-theme.design/
  * Description:       		Extends the functionality of the Merchandiser theme by adding theme specific features.
- * Version:           		2.6
+ * Version:           		2.7
  * Author:            		Get Bowtied
  * Author URI:        		https://getbowtied.com
  * Requires at least: 		6.0
